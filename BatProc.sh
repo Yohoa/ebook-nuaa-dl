2b648da888b52e3b659d3075a9b7943013e3b275
Raphael Yang <308mao@gmail.com>
2017-11-04 23:36:30 +0800

    移除过期成分

--- BatProc.sh
+++
@@ -1,4 +0,0 @@
-python3 BDr.py "http://202.119.70.51:8088/Jpath_sky/DsrPath.do?code=AD262618D2BFA250C05F621FA3DC5BD1&ssnumber=13865827&netuser=1&jpgreadmulu=1&displaystyle=0&channel=0&ipside=0"
-python3 BDr.py "http://202.119.70.51:8088/Jpath_sky/DsrPath.do?code=668C33F2F431EA39A80E3098F50A8864&ssnumber=10536469&netuser=1&jpgreadmulu=1&displaystyle=0&channel=0&ipside=0"
-python3 BDr.py "http://202.119.70.51:8088/Jpath_sky/DsrPath.do?code=AA0B12F9D1BD2A39F9D769415F714CDA&ssnumber=10193158&netuser=1&jpgreadmulu=1&displaystyle=0&channel=0&ipside=0"
-python3 BDr.py "http://202.119.70.51:8088/Jpath_sky/DsrPath.do?code=A6C63B27F2AE67F42E2B0B46231F2F3B&ssnumber=10249163&netuser=1&jpgreadmulu=1&displaystyle=0&channel=0&ipside=0"
